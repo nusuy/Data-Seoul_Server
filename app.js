f0bdb157b69c251423e83de35d43e689ddc43994
nusuy <nsuy.ch@gmail.com>
2023-04-16 07:16:46 +0900

    chore: Add sequelize (#1)

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import db from "./models/index.js";
 
 // env
 dotenv.config();
@@ -7,6 +8,16 @@ dotenv.config();
 // express
 const app = express();
 const PORT = process.env.SERVER_PORT;
+
+// sequelize ORM
+db.sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log(`Database Connected`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
