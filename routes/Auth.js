a3a7a550d9c4ef6b5b560d13bbfd03ec328334bf
nusuy <nsuy.ch@gmail.com>
2023-04-19 22:56:06 +0900

    refactor: Modify module name (#1)
    - from UserController to authController

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,25 +1,25 @@
 import express from "express";
-import userController from "../controllers/userController.js";
+import authController from "../controllers/authController.js";
 import validateAccessToken from "../middlewares/validateAccessToken.js";
 
 const router = express.Router();
 
 // 로그인
-router.post("/login", userController.login);
+router.post("/login", authController.login);
 
 // 소셜 로그인
-router.post("/login/kakao", userController.loginKakao);
+router.post("/login/kakao", authController.loginKakao);
 
 // 로그아웃
-router.post("/logout", validateAccessToken, userController.logout);
+router.post("/logout", validateAccessToken, authController.logout);
 
 // 이메일 가입
-router.post("/join", userController.joinEmail);
+router.post("/join", authController.joinEmail);
 
 // 이메일 인증코드 요청
-router.post("/email", userController.requestEmailCode);
+router.post("/email", authController.requestEmailCode);
 
 // 이메일 인증코드 확인
-router.post("/email/code", userController.verifyEmailCode);
+router.post("/email/code", authController.verifyEmailCode);
 
 export default router;
